12900f395f8d4197db7586f4841e1cd07abcf5ef
gonnaup <gonnaup@163.com>
2022-11-23 20:51:05 +0800

    DateTimeFormatter的一个问题：使用12小时制时，必须将am,pm带上（如：yyyy-MM-dd a hh:mm:ss），否则在解析时formatter会因无法解析time部分而导致LocalDateTime解析失败。但建议使用24小时制（yyyy-MM-dd HH:mm:ss）

--- common/src/main/java/cn/gonnaup/tutorial/common/domain/Order.java
+++ common/src/main/java/cn/gonnaup/tutorial/common/domain/Order.java
@@ -9,11 +9,15 @@ import java.time.LocalDateTime;
 import java.util.Random;
 
 /**
+ * 模拟订单数据
+ *
  * @author gonnaup
  * @version created at 2022/11/20 上午11:13
  */
 public record Order(Integer id, Integer comodityId, int quantity, BigDecimal price, char state,
-                    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss") LocalDateTime createTime) {
+                    //坑：pattern小时使用12小时制 hh 时，必须加上 am,pm匹配符, 即'a'，如"yyyy-MM-dd a hh:mm:ss",
+                    //否则time部分解析不成功，导致LocalDateTime解析失败
+                    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createTime) {
     private static final Faker FAKER = FakerSington.CN.FAKER;
 
     private static final char[] STATE = {'C', 'R', 'F'};
