c9894dd56e77cb19a4a2d07c0e49711ce332a6e0
gonnaup <gonnaup@163.com>
2023-03-09 21:48:16 +0800

    代码清洁

--- springcloud/entry/src/main/java/cn/gonnaup/tutorial/entry/EntryApplication.java
+++ springcloud/entry/src/main/java/cn/gonnaup/tutorial/entry/EntryApplication.java
@@ -3,11 +3,6 @@ package cn.gonnaup.tutorial.entry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
-import java.io.IOException;
-import java.util.Arrays;
 
 /**
  * @author gonnaup
