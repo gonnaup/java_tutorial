3c6c5a9057837987c3c6945f68d62f17f63ffed4
gonnaup <gonnaup@163.com>
2023-09-02 10:40:43 +0800

    feat(版本+doc): 升级spring版本，新建springdoc示例项目
    升级springboot版本到3.1.3，springcloud升级到2022.0.4，新建springdoc示例项目

---
+++ springdoc/src/main/java/org/gonnaup/tutorial/springdoc/SpringdocApplication.java
@@ -0,0 +1,15 @@
+package org.gonnaup.tutorial.springdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gonnaup
+ * @version created at 2023/9/1 下午8:39
+ */
+@SpringBootApplication
+public class SpringdocApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocApplication.class, args);
+    }
+}
